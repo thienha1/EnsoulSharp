efd2ccd2dc94109e36fd456891c7326f92c8665c
thienha1 <hathien69@gmail.com>
2019-10-06 00:28:01 +0700

    Add files via upload

---
+++ LCS_Udyr/LCS_Udyr/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp;
+
+namespace LCS_Udyr
+{
+    static class Helper
+    {
+        public static bool IsTiger(this AIHeroClient unit)
+        {
+            return unit.HasBuff("udyrtigerpunch");
+        }
+        public static bool IsTurtle(this AIHeroClient unit)
+        {
+            return unit.HasBuff("udyrturtleactivation");
+        }
+        public static bool IsBear(this AIHeroClient unit)
+        {
+            return unit.HasBuff("udyrbearactivation");
+        }
+        public static bool IsPhoenix(this AIHeroClient unit)
+        {
+            return unit.HasBuff("udyrphoenixactivation"); // udyrbearstuncheck
+        }
+        public static bool HasBearPassive(this AIHeroClient unit)
+        {
+            return unit.HasBuff("udyrbearstuncheck"); // 
+        }
+    }
+}
---
+++ LCS_Udyr/README.md
@@ -0,0 +1,6 @@
+sharperino
+==========
+
+Some Some Reason i have delete the repo and i will add again all the champions !!!
+
+plz delete all my assemblies and try to install it from this repo!!!
