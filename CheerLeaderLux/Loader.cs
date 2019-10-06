e1a9b11a741f5a054d1a1f4f2f917d989d86bc46
thienha1 <hathien69@gmail.com>
2019-10-06 14:05:11 +0700

    Add files via upload

---
+++ CheerLeaderLux/Loader.cs
@@ -0,0 +1,13 @@
+﻿using EnsoulSharp;
+using EnsoulSharp.Common;
+
+namespace CheerleaderLux
+{
+    class Loader : Lux
+    {
+        static void Main(string[] args)
+        {          
+            CustomEvents.Game.OnGameLoad += OnLoad;
+        }
+    }
+}
---
+++ CheerLeaderLux/Plugins/Extensions/Statics.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EnsoulSharp;
+using EnsoulSharp.Common;
+using SharpDX;
+
+namespace CheerleaderLux.Extensions
+{
+    public class Statics
+    {
+        public static Menu Config;
+        public static readonly List<AIBaseClient> Attackers = new List<AIBaseClient>();
+        public static Orbwalking.Orbwalker Orbwalker;
+        public static Spell Q1;
+        public static Spell E1;
+        public static Spell W1;
+        public static Spell R1;
+        public static GameObject Lux_E;
+        public static SpellSlot Ignite;
+        public static readonly AIHeroClient player = ObjectManager.Player;
+        public static readonly AIHeroClient Player = ObjectManager.Player;
+    }
+}
