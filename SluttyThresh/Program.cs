3837d38564bc20204f01b50a1a1a8fde1945ddaa
thienha1 <hathien69@gmail.com>
2019-10-07 14:31:42 +0700

    Add files via upload

---
+++ SluttyThresh/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnsoulSharp.Common;
+
+
+namespace Slutty_Thresh
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args == null) return;
+            try
+            {
+                CustomEvents.Game.OnGameLoad += SluttyThresh.OnLoad;
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+        }
+    }
+}
