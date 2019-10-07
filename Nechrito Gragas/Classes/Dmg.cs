231cd1adb373efa817f88f5433f671d446727943
thienha1 <hathien69@gmail.com>
2019-10-08 00:24:53 +0700

    Add files via upload

---
+++ Nechrito Gragas/Classes/Dmg.cs
@@ -0,0 +1,28 @@
+﻿using EnsoulSharp;
+using EnsoulSharp.Common;
+
+namespace Nechrito_Gragas
+{
+    class Dmg
+    {
+        public static float ComboDmg(AIBaseClient enemy)
+        {
+            if (enemy != null)
+            {
+                float damage = 0;
+                // if (Logic.HasItem()) damage = damage + (float)Program.Player.GetAutoAttackDamage(enemy) * 0.7f;
+                if (Spells.W.IsReady()) damage = damage + Spells.W.GetDamage(enemy) +
+                              (float)Program.Player.GetAutoAttackDamage(enemy);
+                if (Spells.Q.IsReady()) damage = damage + Spells.Q.GetDamage(enemy);
+                if (Spells.R.IsReady()) damage = damage + Spells.R.GetDamage(enemy);
+                return damage;
+            }
+            return 0;
+        }
+        public static bool IsLethal(AIBaseClient unit)
+        {
+            return ComboDmg(unit) / 1.75 >= unit.Health;
+        }
+    
+}
+}
---
+++ Nechrito Gragas/Classes/Spells.cs
@@ -0,0 +1,26 @@
+﻿using EnsoulSharp;
+using EnsoulSharp.SDK;
+using EnsoulSharp.SDK.Prediction;
+
+namespace Nechrito_Gragas
+{
+    class Spells
+    {
+        private static AIHeroClient Player = ObjectManager.Player;
+        public static SpellSlot Ignite, Smite;
+        public static Spell Q, W, E, R;
+        public static void Initialise()
+        {
+            Q = new Spell(SpellSlot.Q, 775f);
+            W = new Spell(SpellSlot.W);
+            E = new Spell(SpellSlot.E, 600f);
+            R = new Spell(SpellSlot.R, 1050f);
+
+            Q.SetSkillshot(0.3f, 110f, 1000f, false, false, SkillshotType.Circle);
+            E.SetSkillshot(0.15f, 50f, 900f, true, false, SkillshotType.Line);
+            R.SetSkillshot(0.3f, 700f, 1000f, false, false, SkillshotType.Circle);
+
+            Ignite = Player.GetSpellSlot("SummonerDot");
+        }
+    }
+}
